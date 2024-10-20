53f5a7c00a16405a420d2d75f71fba5555128113
ryanhongnguyen <phuocnguyendoublen@gmail.com>
2024-10-19 22:59:52 -0700

    update the email_summarizer_gpt.js

--- gmail_api/email_sender.py
+++ gmail_api/email_sender.py
@@ -31,6 +31,25 @@ if __name__ == '__main__':
     sender = "ryan_hpnguyen@berkeley.edu"
     recipient = "ryan_hpnguyen@berkeley.edu"
     subject = "Hello from Ezemail!"
-    body = "<h1>This is a test email sent from Ezemail's email sender.</h1>"
-
+    body = """
+    <h2>Email Summaries:</h2>
+<ul>
+<li>
+    <strong>Internship opportunity with Walt Disney Imagineering:</strong>
+    <ul>
+        <li><strong>Sender:</strong> Bmc_engineering Departmental</li>
+        <li><strong>Important Information:</strong> *WDI Imaginations* design competition for students passionate about storytelling, innovation, and design. Virtual info session on Thursday (10/17/24).</li>
+        <li><strong>Deadline:</strong> Info session on Thursday (10/17/24)</li>
+    </ul>
+</li>
+<li>
+    <strong>EECS 101: Slight change for CS advising location for drop-in today:</strong>
+    <ul>
+        <li><strong>Sender:</strong> Lydia Raya</li>
+        <li><strong>Important Information:</strong> Drop-in hours from 1:30-2PM in 205 Cory today.</li>
+        <li><strong>Deadline:</strong> Today for drop-in hours</li>
+    </ul>
+</li>
+</ul>"
+"""
     send_email(gmail, sender, recipient, subject, body)
